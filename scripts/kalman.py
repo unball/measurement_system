adb1675602827e7e3eaf47658f1e6a02a8672c30
Vieira Neto <vieiranetoc@gmail.com>
2017-09-18 19:35:26 -0300

    added old system for reference

--- scripts/kalman.py
+++ scripts/kalman.py
@@ -2,29 +2,31 @@
 
 import rospy
 from measurement_system.msg import measurement_msg
-
+from vision.msg import VisionMessage
 
 def main():
-    msg = measurement_msg
-    pub = rospy.Publisher('measurement_system', measurement_msg, queue_size=10)
-    rospy.init_node('measurement_node')
+    global kalman
+    kalman = VisionMessage()
+
+    rospy.init_node('measurement_system', anonymous=True)
     rate = rospy.Rate(10)
-    
-    lista = [x for x in range(6)]
+
+    pub = rospy.Publisher('kalman_topic', VisionMessage, queue_size = 10)
+    rospy.Subscriber('pixel_to_metric_conversion_topic', VisionMessage, subscriber)
+
     try:
-        while not rospy.is_shutdown():
-            msg = measurement_msg()
-            msg.x = lista
-            msg.y = lista
-            msg.th = lista
-            msg.ball_x = 0
-            msg.ball_y = 0
-
-            pub.publish(msg)
-            rate.sleep()
-
+       while not rospy.is_shutdown():
+        pub.publish(kalman)
+        rate.sleep()
     except rospy.ROSInterruptException:
         exit(1)
+
+
+
+def subscriber(data):
+    kalman = data
+
+
 if __name__ == '__main__':
     try:
         main()
