cde6b385e2a882072e2a38ad24418052807b8137
Vieira Neto <vieiranetoc@gmail.com>
2017-09-23 13:36:43 -0300

    fixed pub

--- scripts/kalman.py
+++ scripts/kalman.py
@@ -4,6 +4,15 @@ import rospy
 from measurement_system.msg import measurement_msg
 from vision.msg import VisionMessage
 
+def convert_msg(vision_msg): 
+    measure_msg = measurement_msg()
+    measure_msg.x = vision_msg.x
+    measure_msg.y = vision_msg.y
+    measure_msg.th = vision_msg.th
+    measure_msg.ball_x = vision_msg.ball_x
+    measure_msg.ball_y = vision_msg.ball_y
+    return measure_msg
+
 def main():
     global kalman
     kalman = VisionMessage()
@@ -13,10 +22,10 @@ def main():
 
     pub = rospy.Publisher('kalman_topic', VisionMessage, queue_size = 10)
     rospy.Subscriber('pixel_to_metric_conversion_topic', VisionMessage, subscriber)
-
+    to_pub = convert_msg(kalman)
     try:
        while not rospy.is_shutdown():
-        pub.publish(kalman)
+        pub.publish(to_pub)
         rate.sleep()
     except rospy.ROSInterruptException:
         exit(1)
