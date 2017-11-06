6d8d95313b3eb0e1e5bfdfbc40a4e80001937df4
Samuel Venzi <samuel.venzi@me.com>
2017-11-06 19:42:31 -0200

    Velocity and position calculations

--- scripts/measurement.py
+++ scripts/measurement.py
@@ -4,7 +4,7 @@ import rospy
 from measurement_system.msg import measurement_msg
 from vision.msg import VisionMessage
 
-class DisasseblyMessage(object):
+class DisassemblyMessage(object):
     def __init__(self, message):
         self.x=list(message.x)
         self.y=list(message.y)
@@ -15,21 +15,21 @@ class DisasseblyMessage(object):
 
 
 def estimator(data):
-    local=DisasseblyMessage(data)
+    local = DisassemblyMessage(data)
     for i in xrange(6):
         if not data.found[i] :
-            local.x[i]=estimation.x[i]
-            local.y[i]=estimation.y[i]
-            local.th[i]=estimation.th[i]
+            local.x[i] = estimation.x[i]
+            local.y[i] = estimation.y[i]
+            local.th[i] = estimation.th[i]
+    
     movingAvg(local)
+    ballPredictor(local)
     # unityGain(local)
 
-def ballPrediction(data):
-    k = 10
-    estimation.ball_x_pred = (data.ball_x - estimation.ball_x)*k + data.ball_x
-    estimation.ball_y_pred = (data.ball_y - estimation.ball_y)*k + data.ball_y
+    prev_estimation.ball_x = estimation.ball_x
+    prev_estimation.ball_y = estimation.ball_y
 
-    pass
+
 
 def movingAvg(data):
     alpha = 0.4
@@ -38,13 +38,26 @@ def movingAvg(data):
         estimation.y[i] = (alpha)*estimation.y[i] + (1-alpha)*data.y[i]
         estimation.th[i] = data.th[i]
 
-    # estimation.ball_x = (alpha)*estimation.ball_x + (1-alpha)*data.ball_x
-    # estimation.ball_y = (alpha)*estimation.ball_y + (1-alpha)*data.ball_y
+    estimation.ball_x = (alpha)*estimation.ball_x + (1-alpha)*data.ball_x
+    estimation.ball_y = (alpha)*estimation.ball_y + (1-alpha)*data.ball_y
 
-    ballPrediction(data)
+vel_x = 0.0
+vel_y = 0.0
 
-    estimation.ball_x = data.ball_x
-    estimation.ball_y = data.ball_y
+def ballPredictor(data):
+    global vel_x
+    global vel_y
+    beta = 0.1
+    k = 1
+    vel_x_data = estimation.ball_x - prev_estimation.ball_x
+    vel_y_data = estimation.ball_y - prev_estimation.ball_y
+
+    vel_x = (1-beta)*vel_x_data + (beta)*vel_x
+    vel_y = (1-beta)*vel_y_data + (beta)*vel_y 
+
+    estimation.ball_x_pred = 0.1*(estimation.ball_x + vel_x*k) + 0.9*(data.ball_x)
+    estimation.ball_y_pred = 0.1*(estimation.ball_y + vel_y*k) + 0.9*(data.ball_y)
+
 
 
 
@@ -59,9 +72,9 @@ def unityGain(data):
 
 
 def start():
-    global estimation
-    global vel_x, vel_y
+    global estimation, prev_estimation
     estimation = measurement_msg()
+    prev_estimation = measurement_msg()
 
 
     rospy.init_node('measurement_system', anonymous=True)
